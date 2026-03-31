ea13805f896acb4e39661c5153e37a6646ca6bc3
Sonia Khatun <sonia2khatun4@gmail.com>
2026-03-31 13:54:35 +0530

    update planer data for student tab info

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ from newcontroller.upsert_teacher_chapter_planner import  upsert_teacher_chapter
 from controllers.institute_admin.bulk_teacher_upload_controller_v2 import bulk_upload_users_controller_v2
 
 
-from newcontroller.upsert_teacher_chapter_planner import  upsert_teacher_chapter_planner,get_institute_admin_summary,get_teacher_planer_data, generate_test_from_planner
+from newcontroller.upsert_teacher_chapter_planner import  upsert_teacher_chapter_planner,get_institute_admin_summary,get_teacher_planer_data, generate_test_from_planner, get_student_subjects_tab_info
 # ==========================================
 # 1. SETUP & CONFIGURATION
 # ==========================================
@@ -841,6 +841,10 @@ def bulk_upload_teachers_route():
     return bulk_upload_users_controller_v2()
 
 
+
+@app.route(LEARNING + "/get_student_subjects_tab_info", methods=["GET"])
+def get_student_subjects_tab_info_route():
+    return get_student_subjects_tab_info()
 
 
 # ==========================================
